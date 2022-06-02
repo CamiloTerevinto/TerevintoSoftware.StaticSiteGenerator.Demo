a622bbdb8e9c3dc8c888b579cd5ceb547d31688a
Camilo Terevinto <camiloterevinto@outlook.com>
2022-06-02 08:34:08 +0100

    Continue improving documentation

--- src/CT.Examples.StaticSiteGenerator/CT.Examples.StaticSiteGenerator/Controllers/HomeController.cs
+++ src/CT.Examples.StaticSiteGenerator/CT.Examples.StaticSiteGenerator/Controllers/HomeController.cs
@@ -3,21 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace CT.Examples.StaticSiteGenerator.Controllers;
+
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
     {
         return View();
     }
